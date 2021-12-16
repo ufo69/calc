8b16d654386cfe1ff02427f6b1b450a027cf22a9
ufo69 <58956428+ufo69@users.noreply.github.com>
2021-12-16 22:07:34 +0200

    Add files via upload

---
+++ calc/calc.cpp
@@ -0,0 +1,45 @@
+﻿# include <iostream>
+#include "windows.h"
+using namespace std;
+
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Ievadiet operatoru: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Ievadiet divus skaitļus: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2;
+        Sleep(7000);
+        break;
+
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2;
+        Sleep(7000);
+        break;
+
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2;
+        Sleep(7000);
+        break;
+
+    case '/':
+        cout << num1 << " / " << num2 << " = " << num1 / num2;
+        Sleep(7000);
+        break;
+
+    default:
+        //Ja operators ir cits neka +, -, * or /, parada kļūdas teikumu.
+        cout << "Kļūda! Nepareizs operators!";
+        Sleep(7000);
+        break;
+    }
+
+    return 0;
+}
